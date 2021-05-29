9be96857c2563a4f46f8cb97be1dd3b2abf2a6c7
Timothy Karani <kartimothy@gmail.com>
2021-05-29 07:59:36 +0300

    Can fetch data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -27,7 +29,47 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Child {
+  final int id;
+  final String url;
+  final String updatedAt;
+
+  Child({
+    required this.id,
+    required this.url,
+    required this.updatedAt,
+  });
+
+  factory Child.fromJson(Map<String, dynamic> json) {
+    return Child(
+      id: json["id"],
+      url: json["url"],
+      updatedAt: json["updated_at"],
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  Future<List<Child>> fetchChildren() async {
+    final response = await http.get(Uri.parse(
+        'https://h3k-parent-app-de.a0ijasjbbe5.eu-gb.codeengine.appdomain.cloud/children/'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      List<Child> children = body
+          .map(
+            (dynamic item) => Child.fromJson(item),
+          )
+          .toList();
+
+      return children;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +77,16 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("Hello"),
+        child: FutureBuilder(
+          future: fetchChildren(),
+          builder: (BuildContext context, AsyncSnapshot<List<Child>> snapshot) {
+            if (snapshot.hasData) {
+              return Text("Something is good");
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
